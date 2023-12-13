fcf49d3f4c759db6e162ab2a839e579457152186
Yuliia20Chernova <yuliya20chernova@gmail.com>
2023-12-13 21:17:32 +0100

    check

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const form = document.querySelector("form");
-document.querySelector('[type='button']').addEventListener('click', e=> {
+document.querySelector('[type="submit"]').addEventListener('click', e=> {
     if (!form.checkValidity()) {
         alert('Please, sign up form!');
     }
